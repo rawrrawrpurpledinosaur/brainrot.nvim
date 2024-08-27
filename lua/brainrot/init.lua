68f3d6da122796770b603cf6ced9988c9c5409ec
rawrrawrpurpledinosaur <191292L@student.hci.edu.sg>
2024-08-27 22:12:53 +0800

    its now chatgpt's code

--- lua/brainrot/init.lua
+++ lua/brainrot/init.lua
@@ -1,42 +1,22 @@
 local M = {}
 
-M.config = {
-	auto_start = false,
-	video_source = nil,
-	split_width = 50,
-}
+-- Function to play the video
+function M.play_video()
+	vim.fn.jobstart({ "mpv", "--no-terminal", "--fullscreen", "brainrot.mp4" }, { detach = true })
+end
 
+-- Setup function for user options
 function M.setup(opts)
-	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
-
-	if not M.config.video_source then
-		error("brainrot.nvim: You must specify a video_source in the setup function")
-	end
-
-	if M.config.auto_start then
-		vim.api.nvim_create_autocmd("VimEnter", {
-			callback = function()
-				M.start()
-			end,
-		})
+	opts = opts or {}
+	if opts.auto_play then
+		vim.cmd([[
+      augroup BrainrotAutoPlay
+        autocmd!
+        autocmd VimEnter * lua require('brainrot').play_video()
+      augroup END
+    ]])
 	end
 end
 
-function M.start()
-	-- Create a new split on the right
-	vim.cmd("botright vnew")
-
-	-- Set the width of the new split
-	vim.cmd("vertical resize " .. M.config.split_width)
-
-	-- Disable line numbers and other UI elements in the new buffer
-	vim.wo.number = false
-	vim.wo.relativenumber = false
-	vim.wo.signcolumn = "no"
-
-	-- Use a terminal to play the video
-	local video_source = M.config.video_source
-	vim.fn.termopen("mpv --loop " .. video_source)
-end
-
+-- Return the module
 return M
--- plugin/brainrot.vim
+++ plugin/brainrot.vim
@@ -1,4 +1,4 @@
 if exists('g:loaded_brainrot') | finish | endif 
 let g:loaded_brainrot = 1
 
-command! Brainrot lua require('brainrot').start()
+command! Brainrot lua require('brainrot').play_video()
